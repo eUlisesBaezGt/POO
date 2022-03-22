07c9bd0f303b219e8f4ff0a476ec1a0473e3ad3e
eUlisesBaezGt <0241823@up.edu.mx>
2022-03-22 07:39:53 -0600

    Fibonacci

--- P2/Fibonacci/main.py
+++ P2/Fibonacci/main.py
@@ -1,9 +1,41 @@
-def fibonaaci(n):
-    if n == 0 or n == 1:
-        return n
+# def fibonacci(n):
+#     a, b = 0, 1
+#     while b < n:
+#         print(b, end=' ')
+#         a, b = b, a + b
+#     print()
+#
+#
+# if __name__ == '__main__':
+#     fibonacci(int(input('Enter a number: ')))
+
+# ------------------------------------------------------------------------------ WITHOUT RECURSION
+
+
+# ------------------------------------------------------------------------------ WITH RECURSION
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+#
+#
+# if __name__ == '__main__':
+#     limit = int(input('Enter a number: '))
+#     print(fibonacci(limit))
+#
+# ------------------------------------------------------------------------------ fibonacci
+
+
+def factorial(n):
+    if n == 0:
+        return 1
     else:
-        return fibonaaci(n - 1) + fibonaaci(n - 2)
+        return n * factorial(n - 1)
 
 
 if __name__ == '__main__':
-    print(fibonaaci(10))
+    limit = int(input('Enter a number: '))
+    print(factorial(limit))
