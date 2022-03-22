0de1804446385b4f12a94a52212685e735404e3f
eUlisesBaezGt <0241823@up.edu.mx>
2022-03-22 07:21:07 -0600

    Fibonacci

---
+++ P2/Fibonacci/main.py
@@ -0,0 +1,9 @@
+def fibonaaci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonaaci(n - 1) + fibonaaci(n - 2)
+
+
+if __name__ == '__main__':
+    print(fibonaaci(10))
