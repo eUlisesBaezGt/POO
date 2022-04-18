b8f7e2b6179d5f898ed54eecb3a4bdae1624e040
eUlisesBaezGt <0241823@up.edu.mx>
2022-04-18 10:37:26 -0500

    Update Tk

--- P2/TK/main.py
+++ P2/TK/main.py
@@ -1,5 +1,24 @@
 from tkinter import *
 
-root = Tk()
-rfr = Label(root, text="Hello ")
-root.mainloop()
+window = Tk()
+
+window.title("KA's app")
+window.geometry('700x600')
+
+label = Label(window, text="Kikin Academy")
+label.pack(anchor=CENTER)
+label.config(fg="gold", bg="black", font=("Verdana", 24))
+label.grid(row=0, column=0, columnspan=2)
+
+
+def clicked():
+    label.configure(text="Button was clicked !!")
+
+
+btn = Button(window, text="Click Me", command=clicked)
+btn.grid(row=2, column=0, sticky="nsew", padx=50, pady=50)
+
+btn2 = Button(window, text="#2 Click Me", command=clicked)
+btn2.grid(row=2, column=1, sticky="nsew", padx=50, pady=50)
+
+window.mainloop()
